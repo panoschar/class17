5b3fb104d750ab0e1bd672cb01af4396d6610fc1
panos-charalampopoulos <panos10cha@gmail.com>
2022-01-04 17:15:05 +0200

    init commit

---
+++ askhseis.py
@@ -0,0 +1,27 @@
+#lenght
+str = 'panoscharalampopoulo'
+     #0123456789
+print("Length of str = ", len(str))
+
+#positions
+print (str[2])
+
+#index
+print("First position of the letter a = ", str.index("a"))
+
+#count
+print("It has ", str.count("a"))
+
+#print_in_rows
+print (str[0:20])
+
+
+
+
+
+#next_excersice
+
+str = "HeLLo, hOW aRe YoU?"
+print (str.lower())
+
+print (str.upper())
